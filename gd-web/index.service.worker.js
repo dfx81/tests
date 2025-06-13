15218972a36d65607adbab407d28324946eff4d7
dfx81 <danialfitrighazali@gmail.com>
2025-06-13 15:14:29 +0800

    gd-web fix for mobile

--- gd-web/index.service.worker.js
+++ gd-web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749797364|72352197';
+const CACHE_VERSION = '1749798824|132370663';
 /** @type {string} */
 const CACHE_PREFIX = 'New Game Project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
